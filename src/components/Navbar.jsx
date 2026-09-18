0a8fc1b657aac6878e9a13e8851f84ed1201b410
fiston <kellydigitalmarketing2@gmail.com>
2026-09-18 12:57:39 +0200

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import './Navbar.css';
 const navLinks = [
   { name: 'Services', path: '/#services' },
   { name: 'Work', path: '/#work' },
-  { name: 'Process', path: '/#process' },
   { name: 'About', path: '/#about' },
   { name: 'Insights', path: '/#insights' }
 ];
